6b26e878f5af5e15a58743a7d0cc249712a66e18
Ibrahim <eltabakh987@gmail.com>
2025-08-29 16:56:41 +0300

    add all hooks to cart and products and update structure

--- vite-project/src/layout.jsx
+++ vite-project/src/layout.jsx
@@ -1,18 +1,18 @@
-import Header from '../Components/header'
-import Footer from '../Components/footer'
-import { Outlet } from 'react-router-dom'
-import './layout.css'
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./layout.css";
 
+const queryClient = new QueryClient();
 function Layout() {
-  
-
   return (
-    <>
-   <Header/>
-   <Outlet/>
-   <Footer/>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </QueryClientProvider>
+  );
 }
 
-export default Layout
+export default Layout;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,36 +1,34 @@
-
-import './index.css'
-import { createRoot } from 'react-dom/client'
+import "./index.css";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-//route pages 
-import Layout from './layout.jsx';
-import Home from '../pages/home.jsx';
-import Contact from '../pages/contact.jsx';
-import About from '../pages/about.jsx';
-
+//route pages
+import Layout from "./layout.jsx";
+import Home from "./pages/home.jsx";
+import Contact from "./pages/contact.jsx";
+import About from "./pages/about.jsx";
 
 //create routes my pages
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
-    children:[
-      {index:true,element:<Home/>},
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
       {
-        path:'contact',
-        element:<Contact/>
+        path: "contact",
+        element: <Contact />,
       },
       {
-        path:'about',
-        element:<About/>
+        path: "about",
+        element: <About />,
       },
-    ]
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')).render(
- <>
-      <RouterProvider router={router} />
- </>
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
+  </>
+);
