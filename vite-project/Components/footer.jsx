871ce520ab5b32fd08f40c40febfa2a508e8c295
abduallah <162515003+abuoseed@users.noreply.github.com>
2025-08-30 22:45:52 +0300

    update stracture

--- vite-project/Components/footer.jsx
+++ vite-project/Components/footer.jsx
@@ -3,7 +3,18 @@
 const Footer = () => {
   return (
     <div className='absolute bottom-0'>
-      <h1>footer</h1>
+      <div className="">
+        <div className="">
+          <p>$20 discount for your first order</p>
+        <h1>Join our newsletter and get...</h1>
+        <p>Join our email subscription now to get updates
+           on promotions and coupons.</p>
+        </div>
+        <div className="">
+          <input type='' />
+        </div>
+        
+      </div>
     </div>
   )
 }
